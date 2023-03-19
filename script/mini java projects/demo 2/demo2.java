317b5ddab3cf44814520fada77b7a18a8dec3619
jay shaha <124890353+Jayshaha1009@users.noreply.github.com>
2023-03-19 17:20:04 +0530

    Add files via upload

---
+++ script/mini java projects/demo 2/demo2.java
@@ -0,0 +1,11 @@
+public class demo2 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a = 5;
+        b = 58;
+        c = a * b; // integer number to keep the result of multiplication
+        System.out.println("5*58 = " + c);
+    }
+}
