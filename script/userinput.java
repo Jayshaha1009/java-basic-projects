e6ea9767e8cf5ec0d2c92178c4a1f875ccd722ab
jay shaha <124890353+Jayshaha1009@users.noreply.github.com>
2023-03-19 04:01:05 -0700

    Create userinput.java

---
+++ script/userinput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a integer");
+        int rollno = input.nextInt();
+        System.out.println("your rollno is" + rollno);
+    }
+
+}
